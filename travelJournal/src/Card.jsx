a545e544747b1dd9910a64cd1715285a8807d025
ARoman87 <angel427roman@gmail.com>
2022-11-30 10:41:41 -0500

    Project completed. CSS styling.

--- travelJournal/src/Card.jsx
+++ travelJournal/src/Card.jsx
@@ -7,10 +7,10 @@ function Card(props) {
                 <img src={props.data.img} alt="" />
             </div>
             <div class="cardData">
-                <span><i class="fa fa-map-marker"></i><p>{props.data.location}</p><a href={props.data.maps}>View on Google</a></span>
+                <span><i class="fa fa-map-marker"></i><p class="location">{props.data.location}</p><a href={props.data.maps} target="_blank">View on Google Maps</a></span>
                 <h2>{props.data.locationName}</h2>
                 <h5>{props.data.date}</h5>
-                <p>{props.data.description}</p>
+                <p class="description">{props.data.description}</p>
             </div>
         </div>
     )
